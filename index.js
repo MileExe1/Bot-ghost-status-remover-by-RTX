72c91cb41f0b4e9b22232410de4ee20e8e7608a4
MileExe1 <163154417+MileExe1@users.noreply.github.com>
2024-03-12 15:46:30 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Love Mile","Mile Love All"];
 
 
 let currentIndex = 0;
